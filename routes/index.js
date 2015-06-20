cc07d7cac1598af90d99dc3c979ac48f5b0548eb
pansatoni <pansatorres@gmail.com>
2015-06-20 22:08:00 +0200

    9 Múltiples preguntas

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-//GET /quizes/question
-router.get('/quizes/question',quizController.question);
-
-//GET /quizes/answer
-router.get('/quizes/answer',quizController.answer);
+//Definición de rutas de quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 //GET /author
 router.get('/author',function(req,res){
